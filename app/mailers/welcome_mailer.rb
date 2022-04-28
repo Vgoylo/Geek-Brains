dacdc27f76042313a474f4ea2a060f1044fcdf1f
Vitali <you@example.com>
2022-04-28 16:00:36 +0300

    Add three jog fo sidekiq

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,5 +2,6 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: "welcome" )
+    ThreeJob.perform_at(1.minutes, @user.email)
   end
 end
